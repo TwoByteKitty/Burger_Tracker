c880199fab27e270d2eb7207f5868b41782712d4
Christie Burt <cmburt@yahoo.com>
2019-11-27 19:46:49 -0500

    handlebars working

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express");
-
+const exphbs = require("express-handlebars");
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+//view engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+//listen
 app.listen(PORT, () => {
     console.log(`==> Server listening at http://localhost:${PORT}`)
-})
+});
